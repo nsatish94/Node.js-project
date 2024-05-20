21708885ef154f91f8011f2fe3307c1afebcc1a1
nsatish94 <nsatish111@gmail.com>
2024-05-20 10:13:47 +0000

    url error update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
