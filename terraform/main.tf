d763e46ad60d20fcd1a5576ae3f9004cbed2da5b
nsatish94 <117615816+nsatish94@users.noreply.github.com>
2024-05-21 19:32:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 variable "aws_access_key" {}
@@ -11,9 +9,47 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "public-subnet2"
+  }
+}
+
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "ecs_sg" {
@@ -73,7 +109,7 @@ resource "aws_ecs_task_definition" "hello_world" {
 
   container_definitions = jsonencode([{
     name  = "hello-world"
-    image = "${191545124512.dkr.ecr.us-east-1.amazonaws.com/hello-world:latest}"
+    image = "191545124512.dkr.ecr.us-east-1.amazonaws.com/hello-world"
     portMappings = [{
       containerPort = 3000
       hostPort      = 3000
@@ -89,7 +125,7 @@ resource "aws_ecs_service" "main" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.main.id]
+    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
     security_groups = [aws_security_group.ecs_sg.id]
   }
 }
@@ -99,7 +135,7 @@ resource "aws_lb" "main" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_sg.id]
-  subnets            = [aws_subnet.main.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 }
 
 resource "aws_lb_target_group" "main" {
